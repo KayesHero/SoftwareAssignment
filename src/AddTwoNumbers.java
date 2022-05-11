c2d2dc48f5964b7b0905b017afbf996c6c9b5c93
HamzahRafa <hamzahrafa24@gmail.com>
2022-05-12 00:46:20 +0600

    Added function to allow Users to say 'Hello'

--- src/AddTwoNumbers.java
+++ src/AddTwoNumbers.java
@@ -4,7 +4,10 @@ public class AddTwoNumbers {
 
         int num1 = 5, num2 = 15, sum;
         sum = num1 + num2;
+        int var1;
+
 
         System.out.println("Sum of these numbers: "+sum);
+        System.out.println("hello again");
     }
 }
